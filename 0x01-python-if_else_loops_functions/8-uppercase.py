ab355a7d39839c62f49db76a481512a22cf26215
jormao <jormao@gmail.com>
2019-12-10 16:35:12 -0500

    task8 v1

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new_str = ""
+    for i in range(len(str)):
+        if (97 <= ord(str[i]) <= 122):
+            new_str += chr(ord(str[i]) - 32)
+        else:
+            new_str += str[i]
+    print(new_str)
