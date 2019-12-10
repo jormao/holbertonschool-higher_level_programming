f4fcedde361aea3d93df991a0909cfcc50bce644
jormao <jormao@gmail.com>
2019-12-10 01:45:21 +0000

    task 10, turtle and rabbit

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - checks if there is a loop in the linked list
+ * @list: pointer to head of list
+ * Return: 0 if there isn't a loop, 1 if there is a loop
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *turtle;
+	listint_t *rabbit;
+
+	turtle = list;
+	rabbit = list->next;
+
+	while (1)
+	{
+		if (rabbit == NULL || rabbit->next == NULL)
+			return (0);
+		if (turtle == rabbit)
+			return (1);
+		turtle = turtle->next;
+		rabbit = rabbit->next->next;
+	}
+}
