f7ab52158381ed2594b981d461c1f1e949d14ef6
jormao <jormao@gmail.com>
2019-12-10 14:16:57 -0500

    task 1, check if the last digit of a number an print v2

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,13 +9,16 @@ if (number < 0):
     if (last_digit == 0):
         print("Last digit of {} is 0 and is 0".format(number))
     else:
-        print("Last digit of {} is -{} and is less than 6 and not 0".format(number, last_digit))
+        str = "Last digit of {} is -{} and is less than 6 and not 0"
+        print(str.format(number, last_digit))
 elif (number == 0):
     print("Last digit of {} is 0 and is 0".format(number))
 else:
     if (last_digit == 0):
         print("Last digit of {} is 0 and is 0".format(number))
     elif (last_digit < 6):
-        print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
+        str = "Last digit of {} is {} and is less than 6 and not 0"
+        print(str.format(number, last_digit))
     elif (last_digit > 5):
-        print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
+        str = "Last digit of {} is {} and is greater than 5"
+        print(str.format(number, last_digit))
