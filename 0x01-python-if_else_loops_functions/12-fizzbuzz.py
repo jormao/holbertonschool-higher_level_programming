e1c9f508d89840f56c5e9646924c0e819acad7fe
jormao <jormao@gmail.com>
2019-12-10 17:11:12 -0500

    task12 v2

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -6,6 +6,6 @@ def fizzbuzz():
         elif (i % 5 == 0 and i % 3 != 0):
             print("{:s}".format("Buzz"), end=' ')
         elif (i % 3 == 0 and i % 5 == 0):
-            print("{:s}".format("fizzbuzz"), end=' ')
+            print("{:s}".format("FizzBuzz"), end=' ')
         else:
             print("{:d}".format(i), end=' ')
