8850a6b77a80ba725c9acda85a1bb74ca978bbbc
jormao <jormao@gmail.com>
2019-12-17 17:32:35 -0500

    task 9 fix max_value cant to be 0

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_value = 0
     if len(my_list) == 0:
         return (None)
+    max_value = my_list[0]
     for i in my_list:
         if max_value <= i:
             max_value = i
