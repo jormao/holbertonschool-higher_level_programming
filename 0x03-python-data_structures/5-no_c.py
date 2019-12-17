356d7a67d1331fae89bf6ac3fedb5dc695f6202c
jormao <jormao@gmail.com>
2019-12-17 14:48:22 -0500

    task5 v4

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def no_c(my_string):
     count = 0
-    if my_string == "":
-        return (my_string)
-    for i in my_string:
-        count += 1
-        if i == 'c' or i == 'C':
-            new_string = my_string[:count - 1] + my_string[count:]
-            my_string = new_string
-            count -= 1
-    return (new_string)
+    if my_string:
+        for i in my_string:
+            count += 1
+            if i == 'c' or i == 'C':
+                new_string = my_string[:count - 1] + my_string[count:]
+                my_string = new_string
+                count -= 1
+        return (new_string)
+    return (my_string)
