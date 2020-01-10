d0c87143e39ba9d8281f5460f74227a5348cba86
jormao <jormao@gmail.com>
2020-01-10 05:58:36 +0000

    df

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import dis
+
+
+
+
+
+class MagicClass:
+    def __init__(self, radius=0):
+        if type(radius) is not int or type(radius) is not float:
+            raise TypeError('radius must be a number')
+    def radius(self):
+        return _MagicClass__radius
+
+
+    def area(self, radius):
+        return(pi * (radius ** 2)
+        
