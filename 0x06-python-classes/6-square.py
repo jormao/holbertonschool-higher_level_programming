b8ea1f33068142d2ff2165eac22baf204d599348
jormao <jormao@gmail.com>
2020-01-10 05:13:56 +0000

    Coordinates of a square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+"""
+2-square.py: is a class Square that defines a square
+"""
+
+
+class Square:
+    """class Square that defines a square
+    Attributes:
+        attr1 (size): Private instance attribute size
+        attr2 (area): area of the square
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializer with default size = 0"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        if position[0] is not int or position[1] is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        """Public instance method that returns the current square area"""
+        area = self.__size * self.__size
+        return(area)
+
+    def my_print(self):
+        """Public instance method prints in stdout square with the char #"""
+        if self.__size == 0:
+            print("")
+        for i in range(self.__size):
+            for j in range(self.__size - 1):
+                print("#", end="")
+            print("#")
+
+    @property
+    def size(self):
+        """property to retrieve it"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """property setter to set it"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """property to retrieve it"""
+        return self.__position
+
+    @size.setter
+    def position(self, value):
+        """property setter to set it"""
+        if value[0] is not int or value[1] is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
