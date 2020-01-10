55ce1bca9414de91144ff74faa292c3b6809ace1
jormao <jormao@gmail.com>
2020-01-10 02:43:14 +0000

    v1.2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 """
-0-square.py: is an empty class Square that defines a square:
+1-square.py: is a class Square that defines a square
 """
 
 
@@ -9,15 +9,10 @@ class Square:
     """class Square that defines a square
 
     Attributes:
-        attr1 (str): Description of `attr1`.
-        attr2 (:obj:`int`, optional): Description of `attr2`.
+        attr1 (size): Size of the Square.
 
     """
 
-    def __init__(self, _Square__size):
-        """Initializer with default radius of 1.0"""
-        self._Square__size = _Square__size
-
-    def get_size(_Square__size):
-        """Return the size of this Square"""
-        return self._Square__size
+    def __init__(self, size = 0):
+        """Initializer _Square__size"""
+        self.__size = size
