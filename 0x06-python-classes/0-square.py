61701240783d49d503eb6dce0502fb5458cf0895
jormao <jormao@gmail.com>
2020-01-10 01:10:34 +0000

    My first square v.0.1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 """
-0-square.py: is an empty class Square that defines a square:
+0-square.py: is an empty class Square that defines a square
 """
+
+
 class Square:
     """This is a empty class Square
 
