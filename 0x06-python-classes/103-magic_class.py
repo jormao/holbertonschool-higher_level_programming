7486d94c3917403aa48d8c678cec584d843fc3b8
jormao <jormao@gmail.com>
2020-01-11 07:48:55 +0000

    ByteCode -> Python #5

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+import dis
+
+
+#
+#
+#
 class MagicClass:
     def __init__(self, radius=0):
         self.__radius = 0
@@ -6,8 +12,10 @@ class MagicClass:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
+#
     def area(self):
         return (self.__radius ** 2 * math.pi)
 
+#
     def circumference(self):
         return (2 * math.pi * self.__radius)
