03ede0b3d2183c15ee449c6e1b772ecbb26e3038
jormao <jormao@gmail.com>
2020-01-16 01:51:27 +0000

    fix task 8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -36,7 +36,7 @@ class Rectangle:
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         result_1 = isinstance(rect_1, Rectangle)
-        result_2 = isinstance(rect_1, Rectangle)
+        result_2 = isinstance(rect_2, Rectangle)
         if result_1 is False:
             raise TypeError('rect_1 must be an instance of Rectangle')
         if result_2 is False:
