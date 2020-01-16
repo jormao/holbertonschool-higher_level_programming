571ec22a852055faac84ff2015ed89cd2fc5492c
jormao <jormao@gmail.com>
2020-01-16 01:47:11 +0000

    fix all tasks

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -17,6 +17,14 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initializer with default width and height equal 0"""
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+        if width < 0:
+            raise ValueError('width must be >= 0')
+        if type(height) is not int:
+            raise TypeError('height must be an integer')
+        if height < 0:
+            raise ValueError('height must be >= 0')
         self.__width = width
         self.__height = height
         type(self).number_of_instances += 1
@@ -66,7 +74,7 @@ class Rectangle:
     def height(self, value):
         """to set private instance height"""
         if type(value) is not int:
-            raise TypeError('heidht must be an integer')
+            raise TypeError('height must be an integer')
         if value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value
