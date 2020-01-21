abab00bfd59c3ee0f028fe550d1d7efe3de4a38f
jormao <jormao@gmail.com>
2020-01-21 15:26:09 +0000

    task 6 v.6.0

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+"""
+5-base_geometry.py: Geometry module
+"""
+
+
+class BaseGeometry:
+    """create a empty class"""
+    pass
+
+    def area(self):
+        """public instance method for calculate area"""
+        raise Exception('area() is not implemented')
