c24ebc9635d1aa6f61923eb22b99793c2343ebf6
jormao <jormao@gmail.com>
2020-01-21 06:20:16 +0000

    task 0 v.0.0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+"""
+0-lookup.py: returns the list of available attributes and methods of an object
+
+"""
+
+
+def lookup(obj):
+    """Return list attributes """
+    return dir(obj)
