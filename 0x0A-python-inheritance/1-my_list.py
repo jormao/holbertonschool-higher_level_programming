0fcf6fcdd1fd9dbc431c16b0041fba8790fb6e4d
jormao <jormao@gmail.com>
2020-01-21 06:48:54 +0000

    fix task 1 v.1.2

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -11,5 +11,5 @@ class MyList(list):
 
     def print_sorted(self):
         """prints the list, but sorted (ascending sort)"""
-        if issubclass(Mylist, list):        
+        if issubclass(MyList, list):        
             print(sorted(self))
