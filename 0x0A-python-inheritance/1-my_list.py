c95e2e82ac9bb63d48945fb02a1ead65860480d3
jormao <jormao@gmail.com>
2020-01-21 07:42:49 +0000

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,7 +7,7 @@
 
 
 class MyList(list):
-    """class MyList that inherits from list"""
+    """class MyList that inherits from list and print"""
 
     def print_sorted(self):
         """prints the list, but sorted (ascending sort)"""
