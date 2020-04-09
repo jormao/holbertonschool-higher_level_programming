ecb49d10bdd0e0de26b6f0dbbe3f8a4a633718ea
jormao <jormao@gmail.com>
2020-04-09 13:26:44 -0500

    fix task 5 adding URL

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes URL, sends POST to URL, and displays the body of the response
-curl -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
