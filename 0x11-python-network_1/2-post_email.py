d3e6cd844dfc0e84bdf35a35ced544bd3edca0de
jormao <jormao@gmail.com>
2020-04-14 15:28:40 -0500

    task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+""" script that takes in a URL and an email, sends a POST request
+to the passed URL with the email as a parameter, and displays
+the body of the response (decoded in utf-8)
+"""
+import urllib.parse
+import urllib.request
+import sys
+if __name__ == "__main__":
+    values = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode("utf-8"))
