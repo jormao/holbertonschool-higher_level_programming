9443950457c6fc05ffb2a3c168c0beb3c2b8e279
jormao <jormao@gmail.com>
2020-04-14 16:10:21 -0500

    task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+""" script that takes in a URL, sends a request to the URL and displays the
+body of the response.
+"""
+import requests
+import sys
+if __name__ == "__main__":
+    r = requests.post(sys.argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
