8f5edd34f6ec6898c5e970b7bebb8dd6b2eca09e
jormao <jormao@gmail.com>
2020-04-14 20:12:22 -0500

    task 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,9 +6,7 @@ and uses the Github API to display your id
 import requests
 import sys
 if __name__ == "__main__":
-    auth = ('jormao', 'e28c968ac47559cb867111132d6a0bb807c8bd64')
-    r = requests.get('https://api.github.com/repos/rails/rails/commits',
-                     auth=auth).json()
+    r = requests.get('https://api.github.com/repos/rails/rails/commits').json()
     for _commit in r[:10]:
         author = _commit.get("commit").get("author").get("name")
         sha = _commit.get("sha")
